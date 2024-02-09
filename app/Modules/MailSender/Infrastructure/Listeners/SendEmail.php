a8cd688d9b5c1085b097f7580b7f6e34b7334446
ShaklinDmitry <shaklin.ru@mail.ru>
2024-02-09 15:13:28 +0300

    Связал event создания видео с отправкой письма

---
+++ app/Modules/MailSender/Infrastructure/Listeners/SendEmail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Modules\MailSender\Infrastructure\Listeners;
+
+class SendEmail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        //
+    }
+}
